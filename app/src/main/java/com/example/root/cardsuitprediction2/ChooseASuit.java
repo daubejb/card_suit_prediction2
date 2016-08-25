2d08f1bff6f0c420a5b4452763d51066f0cba3dc
daubejb <daubejb@gmail.com>
2016-08-25 17:22:11 -0400

    create ability to detect accelerometer, write event values to screen

--- app/src/main/java/com/example/root/cardsuitprediction2/ChooseASuit.java
+++ app/src/main/java/com/example/root/cardsuitprediction2/ChooseASuit.java
@@ -1,13 +1,56 @@
 package com.example.root.cardsuitprediction2;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class ChooseASuit extends AppCompatActivity {
+public class ChooseASuit extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    TextView suitView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_asuit);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        suitView = (TextView) findViewById(R.id.suittextview);
+        suitView.setVisibility(View.INVISIBLE);
     }
+
+    @Override
+    protected void onResume() {
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(this, mSensor);
+        super.onPause();
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+
+        String msg = "0: " + event.values[0] + "\n" +
+                "1: " + event.values[1] + "\n" +
+                "2: " + event.values[2] + "\n";
+        suitView.setText(msg);
+        suitView.setVisibility(View.VISIBLE);
+        suitView.invalidate();
+
+    }
+
 }
