120d40de65374aaa6c81a1df74a824ca9aeed9a2
daubejb <daubejb@gmail.com>
2016-08-25 16:40:37 -0400

    create project

---
+++ app/src/main/java/com/example/root/cardsuitprediction2/ChooseASuit.java
@@ -0,0 +1,13 @@
+package com.example.root.cardsuitprediction2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseASuit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_asuit);
+    }
+}
