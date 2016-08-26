fa977ab00f75f53feefce25985f4fa54dc2e6ffc
daubejb <daubejb@gmail.com>
2016-08-26 13:16:19 -0400

    add accelerometer events for clubs, hearts, and diamonds, spades is still a button

--- app/src/main/java/com/example/root/cardsuitprediction2/ChooseASuit.java
+++ app/src/main/java/com/example/root/cardsuitprediction2/ChooseASuit.java
@@ -7,10 +7,13 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ChooseASuit extends AppCompatActivity implements SensorEventListener {
 
+    private final float NOISE = (float) 15.0;
+    private boolean mInitialized; // used for initializing sensor only once
     private SensorManager mSensorManager;
     private Sensor mSensor;
     TextView suitView;
@@ -20,10 +23,33 @@ public class ChooseASuit extends AppCompatActivity implements SensorEventListene
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_asuit);
 
+
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         suitView = (TextView) findViewById(R.id.suittextview);
         suitView.setVisibility(View.INVISIBLE);
+
+        Button clubsButton = (Button)findViewById(R.id.clubsbutton);
+
+        clubsButton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick (View v) {
+                        suitView.setText("YOU WILL CHOOSE CLUBS");
+                        suitView.setVisibility(View.VISIBLE);
+                    }
+                }
+        );
+
+        Button resetButton = (Button)findViewById(R.id.resetbutton);
+
+        resetButton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick (View v) {
+                        suitView.setText("YOU WILL CHOOSE CLUBS");
+                        suitView.setVisibility(View.INVISIBLE);
+                    }
+                }
+        );
     }
 
     @Override
@@ -44,13 +70,75 @@ public class ChooseASuit extends AppCompatActivity implements SensorEventListene
 
     public void onSensorChanged(SensorEvent event) {
 
-        String msg = "0: " + event.values[0] + "\n" +
-                "1: " + event.values[1] + "\n" +
-                "2: " + event.values[2] + "\n";
-        suitView.setText(msg);
-        suitView.setVisibility(View.VISIBLE);
-        suitView.invalidate();
+        // values[0]: Acceleration minus Gx on the x-axis
+        // values[1]: Acceleration minus Gx on the y-axis
+        // values[2]: Acceleration minus Gx on the z-axis
+
+        double x = event.values[0];
+        double y = event.values[1];
+        double z = event.values[2];
+        double mLastX = 0.0;
+        double mLastY = 0.0;
+        double mLastZ = 0.0;
+
+
+        final double ALPHA = 0.8;
+
+
+        double[] gravity = {0, 0, 0};
+
+        // Isolate the force of gravity with the low-pass filter.
+        gravity[0] = ALPHA * gravity[0] + (1 - ALPHA) * event.values[0];
+        gravity[1] = ALPHA * gravity[1] + (1 - ALPHA) * event.values[1];
+        gravity[2] = ALPHA * gravity[2] + (1 - ALPHA) * event.values[2];
+
+        // Remove the gravity contribution with the high-pass filter.
+        x = event.values[0] - gravity[0];
+        y = event.values[1] - gravity[1];
+        z = event.values[2] - gravity[2];
+
+        if (!mInitialized) {
+            // sensor is used for the first time, initialize the last read values
+             mLastX = x;
+             mLastY = y;
+             mLastZ = z;
+            mInitialized = true;
+        } else {
+            // sensor is already initialized, and we have previously read values.
+            // take difference of past and current values and decide which
+            // axis acceleration was detected by comparing values
+
+            double deltaX = Math.abs(mLastX - x);
+            double deltaY = Math.abs(mLastY - y);
+            double deltaZ = Math.abs(mLastZ - z);
+            if (deltaX < NOISE)
+                deltaX = (float) 0.0;
+            if (deltaY < NOISE)
+                deltaY = (float) 0.0;
+            if (deltaZ < NOISE)
+                deltaZ = (float) 0.0;
+
+            mLastX = x;
+            mLastY = y;
+            mLastZ = z;
+
+
+            if (deltaX > deltaY) {
+                suitView.setText("YOU WILL CHOOSE HEARTS");
+                suitView.setVisibility(View.VISIBLE);
+
+            } else if (deltaY > deltaX) {
+                suitView.setText("YOU WILL CHOOSE SPADES");
+                suitView.setVisibility(View.VISIBLE);
+
+            } else if ((deltaZ > deltaX) && (deltaZ > deltaY)) {
+                // Z shake
+                suitView.setText("YOU WILL CHOOSE DIAMONDS");
+                suitView.setVisibility(View.VISIBLE);
+
+            }
+
+        }
 
     }
-
 }
